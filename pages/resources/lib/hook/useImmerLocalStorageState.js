0168761cd9c2eb051a4cbfe2732a8cfc6c12a85c
Fredrik Wictorsson <wictorsson@gmail.com>
2023-07-12 17:35:10 +0200

    deleted webpacks

--- pages/resources/lib/hook/useImmerLocalStorageState.js
+++ pages/resources/lib/hook/useImmerLocalStorageState.js
@@ -1,21 +1,21 @@
-import produce, { freeze } from "immer";
-import { useCallback } from "react";
-import useLocalStorageState from "use-local-storage-state";
+// import produce, { freeze } from "immer";
+// import { useCallback } from "react";
+// import useLocalStorageState from "use-local-storage-state";
 
-export function useImmerLocalStorageState(key, options) {
-  const [value, setValue] = useLocalStorageState(key, {
-    ...options,
-    defaultValue: freeze(options.defaultValue),
-  });
+// export function useImmerLocalStorageState(key, options) {
+//   const [value, setValue] = useLocalStorageState(key, {
+//     ...options,
+//     defaultValue: freeze(options.defaultValue),
+//   });
 
-  return [
-    value,
-    useCallback(
-      (updater) => {
-        if (typeof updater === "function") setValue(produce(updater));
-        else setValue(freeze(updater));
-      },
-      [setValue]
-    ),
-  ];
-}
+//   return [
+//     value,
+//     useCallback(
+//       (updater) => {
+//         if (typeof updater === "function") setValue(produce(updater));
+//         else setValue(freeze(updater));
+//       },
+//       [setValue]
+//     ),
+//   ];
+// }
